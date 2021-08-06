8e20082fa065a897034c9143bdc85af3589fe198
Owen <owensmith321@gmail.com>
2021-08-05 21:38:03 -0400

    brand name refreshes page

--- js/blankpagegenerator.js
+++ js/blankpagegenerator.js
@@ -2,7 +2,7 @@
 document.body.innerHTML = `
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#"><i class="bi-stack" role="img" aria-label="logo"></i>
+    <a class="navbar-brand" href="https://owensmith.page/"><i class="bi-stack" role="img" aria-label="logo"></i>
       <b>Owen</b> Smith</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
