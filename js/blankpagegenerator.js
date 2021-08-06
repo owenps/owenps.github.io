207326a25ccdf20b59e1fb95540f9d73c87c7642
Owen <owensmith321@gmail.com>
2021-08-05 21:31:50 -0400

    refresh

--- js/blankpagegenerator.js
+++ js/blankpagegenerator.js
@@ -2,7 +2,7 @@
 document.body.innerHTML = `
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#"><i class="bi-cpu" role="img" aria-label="logo"></i>
+    <a class="navbar-brand" href="#"><i class="bi-stack" role="img" aria-label="logo"></i>
       <b>Owen</b> Smith</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -11,7 +11,7 @@ document.body.innerHTML = `
     <div class="collapse navbar-collapse" id="navbarColor03">
       <ul class="navbar-nav me-auto">
         <li class="nav-item">
-          <a class="nav-link" id="navHome" href="#">About
+          <a class="nav-link" id="navHome" href="#">Home
             <span class="visually-hidden">(current)</span>
           </a>
         </li>
